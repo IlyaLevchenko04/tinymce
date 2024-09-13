29eec904e8c4e0cf3bbf0ebd0c2f3146de8f5fa5
levchenkoo <i.levchenko@lampalampa.net>
2024-09-13 13:41:23 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Editor } from "@tinymce/tinymce-react";
 
-function App() {
+export default function App() {
+  const [content, setContent] = useState("");
+
+  const handleEditorChange = (content: any, editor: any) => {
+    setContent(content);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Editor
+        apiKey="tvazhq6h0y7bnal9iovhz2x3g6pz1hy8austlwb9a1495y1f"
+        value={content}
+        init={{
+          height: 500,
+          menubar: true,
+          plugins: [
+            "code",
+            "advlist autolink lists link image charmap print preview anchor",
+            "searchreplace visualblocks fullscreen",
+            "insertdatetime media table paste code help wordcount",
+          ],
+          toolbar:
+            "undo redo | formats | bold italic underline strikethrough | code" +
+            "alignleft aligncenter alignright alignjustify | " +
+            "bullist numlist outdent indent | link image | code",
+
+          content_style:
+            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+        }}
+        onEditorChange={handleEditorChange}
+      />
+      <div>
+        <h2>Editor Content</h2>
+        <div dangerouslySetInnerHTML={{ __html: content }} />
+      </div>
     </div>
   );
 }
-
-export default App;
